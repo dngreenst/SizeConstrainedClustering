49f0c6801b02b81c4ee6b6020b6f08fc524db7fa
DANGRE <dangre>
2021-10-24 17:50:12 +0300

    Completed unit tests for the 3 local improvement methods.

--- ClusteringAlgs/test_LocalSearch.py
+++ ClusteringAlgs/test_LocalSearch.py
@@ -189,7 +189,7 @@ class test_LocalSearch(unittest.TestCase):
 
     def test_attempt_moving_agents_between_clusters_agents_are_moved_if_cluster_is_improved(self):
         matrix = np.ones((4, 4))
-        cluster_size = 4
+        cluster_size = 3
 
         agent_to_cluster_map = {0: 0, 1: 0, 2: 1, 3: 1}
 
@@ -205,6 +205,78 @@ class test_LocalSearch(unittest.TestCase):
                 cluster_size=cluster_size,
                 previous_data_loss_score=previous_data_loss,
                 first_agent_index=0,
+                second_agent_index=2)
+
+        self.assertTrue(can_clustering_be_improved_by_moving_an_agent)
+
+    def test_attempt_switching_agents_between_clusters_agents_not_moved_in_the_same_cluster(self):
+        matrix = np.array([[100.0, 100.0, 3.0, 3.0],
+                           [100.0, 100.0, 3.0, 3.0],
+                           [3.0, 3.0, 100.0, 100.0],
+                           [3.0, 3.0, 100.0, 100.0]])
+        cluster_size = 2
+
+        agent_to_cluster_map = {0: 0, 1: 1, 2: 0, 3: 1}
+
+        clustering = [{0, 2}, {1, 3}]
+
+        previous_data_loss = DataLossEstimator.calculate_data_loss(matrix=matrix, clusters=clustering)
+
+        _, _, _, can_clustering_be_improved_by_moving_an_agent = \
+            LocalSearchCluster._attempt_switching_agents_between_clusters(
+                matrix=matrix,
+                clustering=clustering,
+                agent_to_cluster_map=agent_to_cluster_map,
+                cluster_size=cluster_size,
+                previous_data_loss_score=previous_data_loss,
+                first_agent_index=0,
+                second_agent_index=2)
+
+        self.assertFalse(can_clustering_be_improved_by_moving_an_agent)
+
+    def test_attempt_switching_agents_between_clusters_agents_not_moved_if_the_data_loss_is_not_improved(self):
+        matrix = np.ones((4, 4))
+        cluster_size = 2
+
+        agent_to_cluster_map = {0: 0, 1: 0, 2: 1, 3: 1}
+
+        clustering = [{0, 1}, {2, 3}]
+
+        previous_data_loss = DataLossEstimator.calculate_data_loss(matrix=matrix, clusters=clustering)
+
+        _, _, _, can_clustering_be_improved_by_moving_an_agent = \
+            LocalSearchCluster._attempt_switching_agents_between_clusters(
+                matrix=matrix,
+                clustering=clustering,
+                agent_to_cluster_map=agent_to_cluster_map,
+                cluster_size=cluster_size,
+                previous_data_loss_score=previous_data_loss,
+                first_agent_index=0,
+                second_agent_index=2)
+
+        self.assertFalse(can_clustering_be_improved_by_moving_an_agent)
+
+    def test_attempt_switching_agents_between_clusters_agents_are_switched_if_data_loss_is_improved(self):
+        matrix = np.array([[100.0, 100.0, 3.0, 3.0],
+                           [100.0, 100.0, 3.0, 3.0],
+                           [3.0, 3.0, 100.0, 100.0],
+                           [3.0, 3.0, 100.0, 100.0]])
+        cluster_size = 2
+
+        agent_to_cluster_map = {0: 0, 1: 1, 2: 0, 3: 1}
+
+        clustering = [{0, 2}, {1, 3}]
+
+        previous_data_loss = DataLossEstimator.calculate_data_loss(matrix=matrix, clusters=clustering)
+
+        _, _, _, can_clustering_be_improved_by_moving_an_agent = \
+            LocalSearchCluster._attempt_switching_agents_between_clusters(
+                matrix=matrix,
+                clustering=clustering,
+                agent_to_cluster_map=agent_to_cluster_map,
+                cluster_size=cluster_size,
+                previous_data_loss_score=previous_data_loss,
+                first_agent_index=1,
                 second_agent_index=2)
 
         self.assertTrue(can_clustering_be_improved_by_moving_an_agent)
